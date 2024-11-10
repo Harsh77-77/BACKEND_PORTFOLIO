2714bb6df6aece7c915bdaf4c5837899d4fb0771
Harsh Singh <hrsh12-32015@gmail.com>
2024-11-10 17:42:17 +0530

    update

--- api/submit-contact.js
+++ api/submit-contact.js
@@ -6,7 +6,8 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-teal-eight-46.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
